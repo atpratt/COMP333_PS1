2b9e99c04f5fcf3691a66f9605fbd7ebd7f141ac
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-03-08 23:39:28 -0500

    views edit

--- Django/ps2/views.py
+++ Django/ps2/views.py
@@ -8,15 +8,65 @@ def Registration(request):
     retrieval_form = Retrieval_form
     if request.method == 'POST':
         form = Registration_form(request.POST)
-    if form.is_valid():
-        try:
-            user = Users.object.get(username = form.cleaned_data.get("username"))
+        if form.is_valid():
+            try:
+                user = Users.object.get(username = form.cleaned_data.get("username"))
+            except Users.DoesNotExist:
+                user = None
 
-def Song_Retrieval(request):
+            if (form.cleaned_data.get("username") != "" and user == None):
+                new_user = Users(username = form.cleaned_data.get("username"), password = form.cleaned_data.get("password"))
+                new_user.save()
+                registration_form.output = "Successfully registered " + form.cleaned_data.get("username") + "!"
+            elif (form.cleaned_data.get("username") == "" or form.cleaned_data.get("password") == ""):
+                registration_form.output = "You'll need to enter both a username and password!"
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+            else:
+                registration_form.output = "The username '" + form.cleaned_data.get("username") + "' has already been taken.  Please choose a different username."
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        else:
+            context = {'reg_form': registration_form, 'ret_form': retrieval_form}
+
+    return render(request, 'music-db/index.html', context)
+
+
+def Ratings_Retrieval(request):
     registration_form = Registration_form
     retrieval_form = Retrieval_form
     if request.method == 'POST':
         form = Retrieval_form(request.POST)
-    if form.is_valid():
-        try: 
-            ratings = Ratings.objects.filter(username=form.cleaned_data.get("username"))
+        if form.is_valid():
+            try: 
+                ratings = Ratings.objects.filter(username=form.cleaned_data.get("username"))
+                #retrieval_form.output = ""
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form, 'ratings': ratings}
+            except Ratings.DoesNotExist:
+                #retrieval_form.output = ""
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        else:
+            #retrieval_form.output = ""
+            context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+    
+    return render(request, 'music-db/index.html', context)
+
+def Ratings_Retrieval(request):
+    registration_form = Registration_form
+    retrieval_form = Retrieval_form
+    if request.method == 'POST':
+        form = Retrieval_form(request.POST)
+        if form.is_valid():
+            try: 
+                #retrieval_form.output = ""
+                attributes = SongAttributes.objects.filter(name=form.cleaned_data.get("username"))
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form, 'attributes': attributes}
+            except SongAttributes.DoesNotExist:
+                #retrieval_form.output = ""
+                context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        else:
+            #retrieval_form.output = ""
+            context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+    else:
+        #retrieval_form.output = ""
+        context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+
+    return render(request, 'music-db/index.html', context)
