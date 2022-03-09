80486b317cf58998a47dda9818bc09ee62d6b788
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-03-08 21:00:52 -0500

    views edit

--- Django/ps2/views.py
+++ Django/ps2/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .forms import Registration_form, Song_Retrieval_form
-from .models import 
+from .forms import Registration_form, Retrieval_form
+from .models import Users, Artists, Ratings, SongAttributes
 # Create your views here.
 
 def Registration(request):
     registration_form = Registration_form
-    retrieval_form = Song_Retrieval_form
+    retrieval_form = Retrieval_form
     if request.method == 'POST':
         form = Registration_form(request.POST)
     if form.is_valid():
@@ -14,9 +14,9 @@ def Registration(request):
 
 def Song_Retrieval(request):
     registration_form = Registration_form
-    retrieval_form = Song_Retrieval_form
+    retrieval_form = Retrieval_form
     if request.method == 'POST':
-        form = Song_Retrieval_form(request.POST)
+        form = Retrieval_form(request.POST)
     if form.is_valid():
         try: 
             ratings = Ratings.objects.filter(username=form.cleaned_data.get("username"))
