cdcede73b10289175e0068e1a8df5ab0818d13dd
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-03-09 13:52:47 -0500

    models edit

--- Django/ps2/models.py
+++ Django/ps2/models.py
@@ -4,30 +4,34 @@ class Users(models.Model):
     username = models.CharField(max_length = 255, primary_key = True)
     password = models.CharField(max_length = 255)
 
-class Artists(models.Model):
-    song = models.CharField(max_length = 255, primary_key = True)
-    artist = models.ForeignKey(max_length = 255)
-
-class Ratings(models.Model):
-    username = models.ForeignKey(Users, max_length = 255, primary_key = True)
-    song = models.ForeignKey(Artists, max_length = 255)
-    rating = models.IntegerField()
-
 class SongAttributes(models.Model):
-    song = models.ForeignKey(Artists, max_length = 255, primary_key = True)
-    artist = models.ForeignKey(Artists, max_length = 255)
+    song = models.CharField(max_length = 255, primary_key = True)
+    artist = models.CharField(max_length = 255)
     album = models.CharField(max_length = 255)
     genre = models.CharField(max_length = 255)
     year = models.IntegerField(default = 2000)
     record_company = models.CharField(max_length = 255)
 
-    def __str__(self):
+       def __str__(self):
         return (self.song
-                + "" + #self.artist 
+                + "" + self.artist 
                 + "" + self.album
                 + "" + self.genre
                 + "" + self.year
-                + '' + self.record_company)
+                + "" + self.record_company)
+
+class Artists(models.Model):
+    song = models.ForeignKey(SongAttributes, max_length = 255, primary_key = True, on_delete=models.CASCADE)
+    artist = models.ForeignKey(SongAttributes, max_length = 255)
+
+class Ratings(models.Model):
+    username = models.ForeignKey(Users, max_length = 255, primary_key = True, on_delete=models.CASCADE)
+    song = models.ForeignKey(Artists, max_length = 255, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+
+
+ 
     
 
 #unique = true marks them as the primary key
