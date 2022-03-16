05ce9ef5aa8fd54ef0a272ba1370b5dccb4eac79
atpratt <atpratt@wesleyan.edu>
2022-03-16 12:16:28 -0400

    updated registration in views

--- Django/ps2/views.py
+++ Django/ps2/views.py
@@ -5,36 +5,62 @@ from .models import Users, Artists, Ratings, Attributes
 
 #Query the users table
 def Registration(request):
-    registration_form = Registration_form
-    retrieval_form = Retrieval_form
-    if request.method == 'POST':
-        form = Registration_form(request.POST)
-        if form.is_valid():
-            try:
-                user = Users.objects.get(username = form.cleaned_data.get("username"))
-            except Users.DoesNotExist:
-                user = None
-            
-            #if username already in db
-            if (user != None):
-                registration_form.output = "The username '" + form.cleaned_data.get("username") + "' has already been taken.  Please choose a different username."
-            
-            #if username not in db
-            if (form.cleaned_data.get("username") != "" and user == None):
-                new_user = Users.objects.get(username = form.cleaned_data.get("username"))
-                new_user.save()
-                registration_form.output = "Successfully registered " + form.cleaned_data.get("username") + "!"
-            
-            #if either username or password left blank
-            if (form.cleaned_data.get("username") == "" or form.cleaned_data.get("password") == ""):
-                registration_form.output = "You'll need to enter both a username and password!"
+    try:
+        usern=request.POST['username']
+        if usern == "": #no user
+            message = "No information"
+            context = {'message' : message, 'user' : ''}
+            return render(request, 'app/detail.html', context)
+        pswd = request.POST['password']
+        if Users.objects.filter(username=usern).count()==1: 
+            #there already exists a user with this name
+            message = "This Username has been taken"
+            context = {'message' : message, 'user' : ''}
+            return render(request, 'app/detail.html', context)
         
-            context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        u = Users(username=usern, password=pswd)
+        u.save()
 
-        else:
-            context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        message = "New User Created"
+        context = {'message' : message, 'user' : ''}
+        return render(request, 'app/detail.html', context)
+    except(KeyError):
+        message = "Not Availible"
+        context = {'message' : message, 'user' : ''}
+        return render(request, 'app/detail.html', context)
+
+
+# def Registration(request):
+#     registration_form = Registration_form
+#     retrieval_form = Retrieval_form
+#     if request.method == 'POST':
+#         form = Registration_form(request.POST)
+#         if form.is_valid():
+#             try:
+#                 user = Users.objects.get(username = form.cleaned_data.get("username"))
+#             except Users.DoesNotExist:
+#                 user = None
+            
+#             #if username already in db
+#             if (user != None):
+#                 registration_form.output = "The username '" + form.cleaned_data.get("username") + "' has already been taken.  Please choose a different username."
+            
+#             #if username not in db
+#             if (form.cleaned_data.get("username") != "" and user == None):
+#                 new_user = Users.objects.get(username = form.cleaned_data.get("username"))
+#                 new_user.save()
+#                 registration_form.output = "Successfully registered " + form.cleaned_data.get("username") + "!"
+            
+#             #if either username or password left blank
+#             if (form.cleaned_data.get("username") == "" or form.cleaned_data.get("password") == ""):
+#                 registration_form.output = "You'll need to enter both a username and password!"
         
-    return render(request, 'app/detail.html', context)
+#             context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+
+#         else:
+#             context = {'registration_form': registration_form, 'retrieval_form': retrieval_form}
+        
+#     return render(request, 'app/detail.html', context)
     
     # output = " "
     # if form.is_valid():
