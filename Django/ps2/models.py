c1e67bf1c402e300c9266e17df8f0916524bcae2
Wilson McCloy <wilsonmccloy@Wilsons-MacBook-Air.local>
2022-03-16 15:43:48 -0400

    views

--- Django/ps2/models.py
+++ Django/ps2/models.py
@@ -3,11 +3,11 @@ from django.db import models
 #Online it said that we need to add defaults to each element of the table
 #Adding this removed some compiling errors in my terminal, but I am not 100% sure they are correct
 
-class Users(models.Model):
+class User(models.Model):
     username = models.CharField(max_length = 200, primary_key = True, default='SOME STRING')
     password = models.CharField(max_length = 200, default='SOME STRING')
 
-class Attributes(models.Model):
+class Attribute(models.Model):
     name = models.CharField(max_length = 200, primary_key = True, default='SOME STRING')
     album = models.CharField(max_length = 200, default='SOME STRING')
     genre = models.CharField(max_length = 200, default='SOME STRING')
@@ -21,11 +21,11 @@ class Attributes(models.Model):
                 + "" + self.year
                 + "" + self.record_company)
 
-class Artists(models.Model):
+class Artist(models.Model):
     song = models.CharField(max_length = 200, primary_key = True, default='SOME STRING')
     artist = models.ForeignKey(Attributes, max_length = 200, on_delete=models.CASCADE, default='SOME STRING')
 
-class Ratings(models.Model):
+class Rating(models.Model):
     username = models.ForeignKey(Users, max_length = 200, on_delete=models.CASCADE, default='SOME STRING')
     song = models.ForeignKey(Artists, max_length = 200, on_delete=models.CASCADE,default='SOME STRING')
     rating = models.IntegerField(default=2000)
